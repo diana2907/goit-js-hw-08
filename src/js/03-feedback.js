ef1dcdbb48af60bf931ba2970c95223fa18923d6
diana2907 <dinavelkova777@gmail.com>
2022-10-02 22:14:32 +0300

    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,32 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
+
 loadPage();
 form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(evt) {
   evt.preventDefault();
-
+  const formData = {};
   formData[evt.target.name] = evt.target.value;
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function loadPage() {
-  if (localStorage.getItem(STORAGE_KEY)) {
-    const parsed = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const parsed = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parsed.email) {
     input.value = parsed.email;
+  }
+  if (parsed.message) {
     textarea.value = parsed.message;
   }
 }
+
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
